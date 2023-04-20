47bde5848416705bc1c3146e40854fff07861cd6
Adrian Begg <adriabeg@amazon.com>
2023-04-20 13:22:51 +0200

    fix: adjusted examples to enable tfvars

--- examples/existing_environment/main.tf
+++ examples/existing_environment/main.tf
@@ -2,20 +2,20 @@ module "refactor_spaces_microservice_x" {
   source  = "aws-ia/refactor-spaces/aws"
   version = "0.1.0"
 
-  environment_id = "env-ab9ZXYVT9z"
-  application_id = "app-xy1ABCDE1a"
+  environment_id = var.environment_id
+  application_id = var.application_id
   services = [
     {
-      application_name = "Unistore"
-      name             = "Unistore-AddReview"
-      description      = "The new AddReview Microservice"
+      application_name = var.application_name
+      name             = var.display_name_lambda_microservice
+      description      = var.description_lambda_microservice
       endpoint_type    = "LAMBDA"
-      lambda_arn       = "arn:aws:lambda:us-east-1:999999999999:function:AddReview"
+      lambda_arn       = var.lambda_arn_microservice
       routes = [
         {
-          source_path         = "/unicorns/reviews"
+          source_path         = var.source_path_lambda_microservice
           include_child_paths = true
-          http_verbs          = ["POST", "GET"]
+          http_verbs          = var.http_verbs_lambda_microservice
           route_active        = true
         }
       ]
